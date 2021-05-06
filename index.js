23a5178960fbd748a45349c3659bcd28bcae9e1b
lukevangorder <lukevangorder@gmail.com>
2021-05-06 17:28:47 -0400

    commit tree eb6c4d70f697fab297e029364951fb2d9f4c4293

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const fd = function(drivers) {
+    return drivers.slice(0,1);
+}
+
+const ld = function(drivers) {
+    return [drivers.size-2 , drivers.size-1];
+}
+
+const selectingDrivers = [fd, ld];
+
+function createFareMultiplier(mult) {
+    return function(fare) {
+        return fare * mult;
+    }
+}
+
+const df = function(fare) {
+    return createFareMultiplier(2)(fare);
+}
